a81dc15bbaeb3439d821897269888e779c853ecd
gynura <josep97mg@gmail.com>
2023-04-24 14:45:33 +0200

    added sakura-spaceship-theme

---
+++ zsh/sakura-spaceship-theme/.spaceshiprc.zsh
@@ -0,0 +1,19 @@
+# SPACESHIP THEME CONFIG
+SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  line_sep      # Line break
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_USER_SHOW=always
+SPACESHIP_DIR_TRUNC=0
+SPACESHIP_DIR_TRUNC_REPO=true
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL_ROOT="#"
+SPACESHIP_CHAR_SYMBOL="🌸"
+SPACESHIP_CHAR_SUFFIX=" "
